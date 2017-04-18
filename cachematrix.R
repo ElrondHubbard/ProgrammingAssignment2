7717b66f0a825dd0068314d90af9c800787fcd20
Elrond Hubbard <elrond.hubbard2@gmail.com>
2017-04-18 17:32:37 -0500

    R Programming - Assignment 2
    Annotated script for the makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,63 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    ## Get value of matrix
+    get <- function()x
+    
+    ## Set value of Inverse
+    setInverse <- function(inverse) inv <<- inverse
+    
+    ## Get value of Inverse
+    getInverse <- function() inv
+    list(
+        set = set, 
+        get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+
+    ## Run function - check to see if it's been solved already
+    inv <- x$getInverse()
+    
+    ## If inverse is not null (has been solved), display cute message, print cache
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    
+    ## If there is no cache (has not been solved), derive inverse, set value, cache value
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setInverse(inv)
+    inv
 }
+
+## Series of tests to see if functions work properly
+test_data <- makeCacheMatrix(matrix(1:4, 2,2))
+test_data$get()
+test_data$getInverse()
+cacheSolve(test_data)
+cacheSolve(test_data)
+test_data$getInverse()
+test_data$set(matrix(c(2,2,1,4),2,2))
+test_data$get()
+test_data$getInverse()
+cacheSolve(test_data)
+cacheSolve(test_data)
+test_data$getInverse()
+
+## Unless I'm really missing something, it works. 
+## Final piece removes functions and test data.
+rm(test_data, cacheSolve, makeCacheMatrix)
